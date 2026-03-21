c16273d09b0f2b3190cb3c507d8df92be1b8ab4b
Luis Daniel Lucio Quiroz <dieu@andres.okay.com.mx>
2026-03-21 07:24:15 -0400

    vendors, permissions and settings first attempt

---
+++ app2yaml.php
@@ -0,0 +1,88 @@
+<?php
+$short = "v";
+$long = ['file:','directory:','type:'];
+$o = getopt($short, $long);
+
+if (isset($o['v'])){
+    var_dump($o);
+}
+
+if (isset($o['file']) && isset($o['directory']))
+    die('the parameters --file or --directory can not coexist.'.PHP_EOL);
+
+if (!isset($o['type']))
+    die('you must specify --type with one of the following options: vendors, permissions, settings, menu.'.PHP_EOL);
+
+if (isset($o['file'])){
+    $x = 0;
+    require_once($o['file']);
+
+    if (isset($o['v'])){
+        print_r($apps);
+        print_r($vendors);
+    }
+}
+elseif(isset($o['directory'])){
+    $files = glob($o['directory'].'/{core,app}/*/app_config.php', \GLOB_BRACE);
+    if (isset($o['v'])){
+        var_dump($files);
+    }
+    $x = 0;
+    foreach ($files as $file){
+        require_once($file);
+        $x++;
+    }
+
+    if (isset($o['v'])){
+        print_r($apps);
+        print_r($vendors);
+    }
+}
+else
+    die('--file or --directory are missing.'.PHP_EOL);
+
+switch ($o['type']){
+    case 'vendors':
+        if (isset($vendors) && is_array($vendors)){
+            $yaml = yaml_emit($vendors);
+        }
+        break;
+    case 'permissions':
+        $permissions = [];
+        if (isset($apps[0]) && is_array($apps[0])){
+            foreach ($apps as $app){
+                if (is_array($app['permissions']))
+                    foreach ($app['permissions'] as $permission)
+                        $permissions[] = $permission;
+            }
+        }
+
+        if (isset($o['v'])){
+            print_r($permissions);
+        }
+
+        $yaml = yaml_emit($permissions);
+        break;
+    case 'settings':
+        $settings = [];
+        if (isset($apps[0]) && is_array($apps[0])){
+            foreach ($apps as $app){
+                if (is_array($app['default_settings']))
+                    foreach ($app['default_settings'] as $setting)
+                        $settings[] = $setting;
+            }
+        }
+
+        if (isset($o['v'])){
+            print_r($settings);
+        }
+
+        $yaml = yaml_emit($settings);
+        break;
+    case 'menu':
+        break;
+    default:
+        die('you must specify --type with one of the following options: vendors, permissions, settings, menu.'.PHP_EOL);
+}
+
+echo $yaml;
